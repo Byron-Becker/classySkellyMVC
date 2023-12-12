ca8218d3eade9bb34cdd8b70432a7be911ca5daf
bbeck9123 <byron.ptgo@gmail.com>
2023-12-11 18:48:44 -0800

    able to post w/  measurement swap

--- controllers/measurements.js
+++ controllers/measurements.js
@@ -1,10 +1,10 @@
 const cloudinary = require("../middleware/cloudinary");
-const Post = require("../models/Post");
+const Measurement = require("../models/Measurement");
 
 module.exports = {
   getProfile: async (req, res) => {
     try {
-      const posts = await Post.find({ user: req.user.id });
+      const posts = await Measurement.find({ user: req.user.id });
       res.render("profile.ejs", { posts: posts, user: req.user });
     } catch (err) {
       console.log(err);
@@ -18,20 +18,20 @@ module.exports = {
       console.log(err);
     }
   },
-  getPost: async (req, res) => {
+  getMeasurement: async (req, res) => {
     try {
-      const post = await Post.findById(req.params.id);
+      const post = await Measurement.findById(req.params.id);
       res.render("post.ejs", { post: post, user: req.user });
     } catch (err) {
       console.log(err);
     }
   },
-  createPost: async (req, res) => {
+  createMeasurement: async (req, res) => {
     try {
       // Upload image to cloudinary
       const result = await cloudinary.uploader.upload(req.file.path);
 
-      await Post.create({
+      await Measurement.create({
         title: req.body.title,
         image: result.secure_url,
         cloudinaryId: result.public_id,
---
+++ routes/measurements.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/multer");
+const measurementsController = require("../controllers/measurements");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Post Routes - simplified for now
+router.get("/:id", ensureAuth, measurementsController.getMeasurement);
+
+router.post("/createPost", upload.single("file"), measurementsController.createMeasurement);
+
+router.put("/likePost/:id", measurementsController.likePost);
+
+router.delete("/deletePost/:id", measurementsController.deletePost);
+
+module.exports = router;
