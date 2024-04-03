5b29e19edc6749dc5eca79336b41671e92e7e929
bbeck9123 <byron.ptgo@gmail.com>
2024-04-03 12:14:37 -0700

    newest

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./views/profile.ejs', './views/partials/header.ejs'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
